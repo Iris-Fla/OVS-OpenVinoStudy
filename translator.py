eba5dbb082efc17c71a37a2dc9cc44d20c56197d
Iris-Fla <irith.fla@gmail.com>
2024-05-13 16:02:19 +0900

    0513

---
+++ translator.py
@@ -0,0 +1,20 @@
+from transformers import pipeline
+
+class EnJaTranslator:
+
+    def __init__(self,model_name="staka/fugumt-en-ja"):
+        """翻訳モデルロード
+        Params:
+            model_name(str): huggingfaceの翻訳モデル名
+        """
+        self.trans = pipeline("translation", model=model_name)
+    
+    def __call__(self,text):
+        """翻訳結果の文字列を返す
+        Params:
+            text(str): 英語の文字列
+        Returns:
+            str: 翻訳後の日本語の文字列
+        """
+        result = self.trans(text)
+        return  result[0]['translation_text']
